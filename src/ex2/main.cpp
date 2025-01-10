bd6f84cf03d06f34ffcbf71717e39f95df0fade2
steve vallois-davies <stevie.vallois.davies@gmail.com>
2025-01-10 15:58:01 +0000

    feat: show texture

--- src/ex2/main.cpp
+++ src/ex2/main.cpp
@@ -8,6 +8,7 @@
 #include <filesystem>
 
 #include "utility.h"
+#include "stb_image.h"
 #include "shader_program.hpp"
 
 const char *APP_TITLE = "Introduction to Modern OpenGL - Hello Colored Triangle";
@@ -15,12 +16,94 @@ const int gWindowWidth = 800;
 const int gWindowHeight = 600;
 GLFWwindow *gWindow = NULL;
 
+std::filesystem::path shaderDir = getEnvVar("SHADERS_DIR");
+std::filesystem::path assetsDir = getEnvVar("ASSETS_DIR");
 int main()
 {
+
     if (!initOpengl(gWindow, gWindowWidth, gWindowHeight))
+    {
+        std::cerr << "glfw initialisation failed" << std::endl;
+        return -1;
+    }
+
+    std::filesystem::path shaderDir = getEnvVar("SHADERS_DIR");
+    ShaderProgram s(shaderDir);
+    if (!s.loadShaders("02_shader.vs", "02_shader.fs"))
     {
         return 1;
     }
-    
+
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+    };
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // postion attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(sizeof(GLfloat) * 3));
+    glEnableVertexAttribArray(1);
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(sizeof(GLfloat) * 6));
+    glEnableVertexAttribArray(2);
+
+    // glBindTexture(GL_TEXTURE_2D, texture);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int w, h, nrChannels;
+    std::string t = assetsDir / "container.jpg";
+    unsigned char *data = stbi_load(t.c_str(), &w, &h, &nrChannels, 0);
+    if (data == NULL)
+    {
+        std::cout << "Failed to load texture" << std::endl;
+        return 1;
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+    while (!glfwWindowShouldClose(gWindow))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        s.use();
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glfwSwapBuffers(gWindow);
+        glfwPollEvents();
+    }
+
     return 0;
 }
