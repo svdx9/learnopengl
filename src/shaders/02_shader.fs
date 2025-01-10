44f34aa18f7e3b7020ef2ba7c9fb3fe20cc7eabc
Steve Vallois-Davies <svalloisdavies@drw.com>
2025-01-10 18:36:53 +0000

    feat: texture flip

--- src/shaders/02_shader.fs
+++ src/shaders/02_shader.fs
@@ -8,5 +8,8 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    // stbi_set_flip_vertically_on_load(true);
+    // make the happy face looks in the other/reverse direction by changing the fragment shader:
+    // FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x * -1, TexCoord.y * -1)), 0.2);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.3);
 }
